b6c36d9d8fdd071229e0d0fec8fd54c14a9a4856
Yasmim Marques <yasmim.msantos@outlook.com>
2021-12-13 16:40:56 -0300

    Aplicando security

--- blogPessoal - Backend/src/main/java/org/generation/blogPessoal/model/Tema.java
+++ blogPessoal - Backend/src/main/java/org/generation/blogPessoal/model/Tema.java
@@ -9,7 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
@@ -20,7 +20,7 @@ public class Tema {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	@NotNull
+	@NotBlank (message = "o atributo descrição não pode estar vazio")
 	private String descricao;
 	@OneToMany(mappedBy = "tema", cascade = CascadeType.ALL)
 	@JsonIgnoreProperties("Tema")
